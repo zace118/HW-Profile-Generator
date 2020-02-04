5abe1976f3f4778cdd6ef9fe7b7ba7eb721290e0
Zac Eskridge <zace118@gmail.com>
2020-02-04 00:06:50 -0700

    Good lord. No idea why inquirer isn't working. Nothing works. Everything is broken, including my spirit. Bye

--- app.js
+++ app.js
@@ -1,3 +1,37 @@
 const inquirer = require('inquirer'),
     jest = require('jest'),
     fs = require('fs');
+
+
+// Inquirer inquiring user for name, ID# and job title
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'What is your name?',
+            name: 'name'
+        },
+        {
+            type: 'input',
+            message: 'What is your ID number?',
+            name: 'id'
+        },
+        {
+            type: 'list',
+            message: 'What is your title?',
+            name: 'title',
+            choices: [
+                'Intern',
+                'Employee',
+                'Engineer',
+                'Manager'
+            ]
+
+        }
+    ]).then(function (res) {
+        console.log(res);
+
+
+    }).catch(function (err) {
+        console.log(err);
+    })
