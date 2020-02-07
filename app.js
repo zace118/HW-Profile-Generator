3b63d20ea25e7d96c663a227bf7195adbec78b8e
Zac Eskridge <zace118@gmail.com>
2020-02-07 00:51:53 -0700

    added in next questions

--- app.js
+++ app.js
@@ -2,35 +2,90 @@ const inquirer = require('inquirer'),
     fs = require('fs');
 
 
+let team = [];
+
 // Inquirer inquiring user for name, ID# and job title
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            message: 'What is your name?',
-            name: 'name'
-        },
-        {
-            type: 'input',
-            message: 'What is your email address?',
-            name: 'email'
-        },
-        {
-            type: 'list',
-            message: 'What is your title?',
-            name: 'title',
-            choices: [
-                'Intern',
-                'Employee',
-                'Engineer',
-                'Manager'
-            ]
+function initialPrompt() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'What is your name?',
+                name: 'name'
+            },
+            {
+                type: 'input',
+                message: 'What is your email address?',
+                name: 'email'
+            },
+            {
+                type: 'list',
+                message: 'What is your title?',
+                name: 'title',
+                choices: [
+                    'Intern',
+                    'Engineer',
+                    'Manager'
+                ]
+            },
+            {
+                type: 'input',
+                message: 'What school do you go to?',
+                name: 'school',
+                when: function (answers) {
+                    return answers.title === "Intern";
+                }
+            },
+            {
+                type: 'input',
+                message: 'What is your GitHub user name?',
+                name: 'github',
+                when: function (answers) {
+                    return answers.title === "Engineer";
+                }
+            },
+            {
+                type: 'input',
+                message: 'What is your office number?',
+                name: 'officeNumber',
+                when: function (answers) {
+                    return answers.title === "Manager";
+                }
+            }
 
-        }
-    ]).then(function (res) {
-        console.log(res);
+        ]).then(function (res) {
+            console.log(res);
+            team.push(res);
+
+            moreTeam();
+
+        }).catch(function (err) {
+            console.log(err);
+        })
+};
+
+initialPrompt();
 
 
-    }).catch(function (err) {
-        console.log(err);
-    })
+function moreTeam() {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                message: 'Are there more team members?',
+                name: 'moreTeam',
+                choices: [
+                    'Yes',
+                    'No'
+                ]
+            }
+        ]).then(function (res) {
+            console.log(res.moreTeam);
+            if (res.moreTeam === 'Yes') {
+                initialPrompt();
+            }
+            else {
+                console.log('Beautimous');
+            }
+        })
+};
